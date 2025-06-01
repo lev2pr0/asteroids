5717d37349f920a790fac919e3c61d153476a554
Levar Norwood <levar.norwood@outlook.com>
2025-06-01 19:08:58 -0400

    L2: Game Loop

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+from constants import *
+
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
 import pygame
 
-from constants import *
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def gameloop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, BLACK)
+        pygame.display.flip()
 
 def main():
+    pygame.init()
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    gameloop()
 
 
 
