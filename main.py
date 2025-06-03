59532bd243e1c88ad4fd86ed5cb5f6fa487b3047
Levar Norwood <levar.norwood@outlook.com>
2025-06-02 20:00:30 -0400

    L3: FPS

--- main.py
+++ main.py
@@ -6,17 +6,24 @@ from constants import *
 import pygame
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+
 
 def gameloop():
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         pygame.Surface.fill(screen, BLACK)
         pygame.display.flip()
+        dt += clock.tick(60)/1000
 
 def main():
     pygame.init()
+
+
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
